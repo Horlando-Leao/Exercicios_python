9955f4e3510f74bdae5018baf29cad065ab481e8
Horlando Leão <horlandoleao8@gmail.com>
2020-11-02 23:26:12 -0300

    Update distanciaEntreDoisPontos_cartesiano.py

--- distanciaEntreDoisPontos_cartesiano.py
+++ distanciaEntreDoisPontos_cartesiano.py
@@ -10,7 +10,7 @@ cordernadaY_B = float(input("Posição de Y para B: "))
 pontoA = [cordernadaX_A,cordernadaY_A]
 pontoB = [cordernadaX_B,cordernadaY_B]
 
-#formula para calcular distância entre dois pontos no cartesiano, sem raiz
+#formula para calcular distância entre dois pontos no cartesiano
 # distancia = √(x_b - x_a)² + (y_b - y_a)² 
 distancia = math.sqrt( ( (pontoB[0] - pontoA[0]) ** 2) + ( (pontoB[1] - pontoA[1]) ** 2) )
 print(distancia)
